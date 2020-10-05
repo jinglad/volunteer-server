191d0d53a811f4214e245f42c5a15cbbd23dadb8
jinglad <57634584+jinglad@users.noreply.github.com>
2020-10-05 16:49:00 +0600

    commit

--- index.js
+++ index.js
@@ -15,85 +15,60 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-app.post('/addEvent', (req, res) => {
-    const event = req.body;
-    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        const eventsCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLL_1);
+client.connect(err => {
+    const eventsCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLL_1);
+    const volunteersCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLL_2);
+
+    app.get('/', (req, res) => {
+        res.send('welcome back')
+    })
+
+    app.post('/addEvent', (req, res) => {
+        const event = req.body;
         eventsCollection.insertMany(event)
             .then(result => {
                 console.log(result.insertedCount);
                 res.send(result.insertedCount);
-            });
-        client.close();
-    });
-});
+            })
+    })
 
-
-app.get('/events', (req, res) => {
-    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
+    app.get('/events', (req, res) => {
         eventsCollection.find({})
             .toArray((err, documents) => {
                 res.send(documents);
-            });
-        client.close();
+            })
     })
-});
 
-app.post('/addVolunteer', (req, res) => {
-    const volunteer = req.body;
-    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        const volunteersCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLL_2);
+    app.post('/addVolunteer', (req, res) => {
+        const volunteer = req.body;
         volunteersCollection.insertOne(volunteer)
             .then(result => {
                 res.send(result.insertedCount > 0);
-            });
-        client.close();
-    });
-});
+            })
+    })
 
-app.get('/registeredEvents', (req, res) => {
-    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        const volunteersCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLL_2);
+    app.get('/registeredEvents', (req, res) => {
         volunteersCollection.find({ email: req.query.email })
             .toArray((err, documents) => {
                 res.send(documents);
-            });
-        client.close();
+            })
     })
-});
 
-app.get('/volunteers', (req, res) => {
-    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        const volunteersCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLL_2);
+    app.get('/volunteers', (req, res) => {
         volunteersCollection.find({})
             .toArray((err, documents) => {
                 res.send(documents);
-            });
-        client.close();
-    });
-});
+            })
+    })
 
-app.delete('/delete/:id', (req, res) => {
-    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        const volunteersCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLL_2);
+    app.delete('/delete/:id', (req, res) => {
         volunteersCollection.deleteOne({ _id: ObjectId(req.params.id) })
             .then(result => {
                 res.send(result.deletedCount > 0);
-            });
-        client.close();
-    });
-
-})
-
-app.get('/', (req, res) => {
-    res.send('welcome back')
+            })
+    })
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT);
